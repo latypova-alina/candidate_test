50d6e2b951a34f01eefcae5132f9d68742323986
Alina Latypova <alina.latypova@flatstack.com>
2022-05-23 15:19:23 +0200

    add scopes for orders

--- app/jobs/manage_orders_job.rb
+++ app/jobs/manage_orders_job.rb
@@ -10,18 +10,18 @@ class ManageOrdersJob < ApplicationJob
       @logs = "Nightly routine for customer #{customer.id}...\n"
 
       # All the orders that have been delivered
-      customer.orders.where(active: true).where("delivered_at IS NOT NULL").each do |order|
+      customer.orders.delivered.each do |order|
         order.update_attribute(:active, false)
       end
 
-      # All the orders that are paid, but need delivery
-      customer.orders.where(active: true).where("paid_at IS NOT NULL AND shipped_at IS NULL").each do |order|
+      # All the orders that are paid, but need delivery
+      customer.orders.paid.each do |order|
         order.product.ship_for(customer) unless simulate?
         order.update_attribute(:shipped_at, Time.now) unless simulate?
       end
 
-      # All the orders that are not paid
-      customer.orders.where(active: true).where("paid_at IS NULL").each do |order|
+      # All the orders that are not paid
+      customer.orders.not_paid.each do |order|
         if customer.orders.where("created_at > ?", Date.today.at_midnight).any?
           customer.send_invoice(order) unless simulate?
           @logs << "Invoice sent\n"
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :product
+
+  scope :active, -> { where(active: true) }
+  scope :delivered, -> { active.where.not(delivered_at: nil) }
+  scope :paid, -> { active.where.not(paid_at: nil).where(shipped_at: nil) }
+  scope :not_paid, -> { active.where(paid_at: nil) }
 end
