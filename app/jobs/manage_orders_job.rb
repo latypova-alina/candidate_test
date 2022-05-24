566dd63b2187117d8c13f2f1542147ed504881e2
Alina Latypova <alina.latypova@flatstack.com>
2022-05-24 20:40:40 +0200

    add common method for logging errors

--- app/jobs/manage_orders_job.rb
+++ app/jobs/manage_orders_job.rb
@@ -4,43 +4,68 @@ require "payment_reminder"
 
 class ManageOrdersJob < ApplicationJob
   include ApplicationHelper
-  include ManageOrderJobHelper
   queue_as :default
 
   def perform(*_args)
-    process_delivered_orders
-    process_paid_orders
-    process_unpaid_orders
+    perform_delivered_orders
+    perform_paid_orders
+    perform_unpaid_orders
 
     Customer.all.each { |customer| log(I18n.t("routine_message", customer_id: customer.id)) }
   end
 
   private
 
-  def process_delivered_orders
+  def perform_delivered_orders
     Order.delivered.update_all({ active: false })
   end
 
-  def process_paid_orders
+  def perform_paid_orders
     return if simulate?
 
-    paid_orders.includes(:product, :customer).each do |order|
-      order.product.ship_for(order.customer)
-    end
+    ship
     paid_orders.update_all({ shipped_at: Time.now })
   end
 
-  def process_unpaid_orders
-    new_orders = not_paid_orders.where("created_at > ?", Date.today.at_midnight)
-    old_orders = not_paid_orders.where.not(id: new_orders)
+  def perform_unpaid_orders
+    process_new_orders
+    process_old_orders
+  end
 
-    new_orders.includes(:customer).each do |order|
-      send_invoice(order)
-    end
+  def process_new_orders
+    new_orders = not_paid_orders
+      .where("created_at > ?", Date.today.at_midnight)
+      .includes(:customer)
 
-    old_orders.includes(:customer).each do |order|
-      send_reminder(order)
+    process_orders(new_orders, "invoice_error_message") { send_invoice(@order) }
+  end
+
+  def process_old_orders
+    new_orders = not_paid_orders
+      .where("created_at > ?", Date.today.at_midnight)
+      .includes(:customer)
+
+    old_orders = not_paid_orders
+      .where.not(id: new_orders)
+      .includes(:customer)
+
+    process_orders(old_orders, "reminder_error_message") { send_reminder(@order) }
+  end
+
+  def ship
+    process_orders(paid_orders.includes(:product, :customer), "ship_error_message") do
+      @order.product.ship_for(@order.customer)
     end
+  end
+
+  def process_orders(orders, error)
+    orders.each do |order|
+      @order = order
+
+      yield
+    end
+  rescue
+    log(I18n.t(error, order_id: @order.id))
   end
 
   def paid_orders
@@ -53,11 +78,13 @@ class ManageOrdersJob < ApplicationJob
 
   def send_invoice(order)
     order.customer.send_invoice(order) unless simulate?
+
     log(I18n.t("invoice_sent"))
   end
 
   def send_reminder(order)
     PaymentReminder.new(order).send
+
     log(I18n.t("reminder_sent"))
   end
 
